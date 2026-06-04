1ab6c81369ce82251cc036e26bece2aab5d46d25
Sachin Rawat <logistics@qurvii.com>
2026-06-04 18:08:26 +0530

    Update URL for Raw Material Inventory

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -80,7 +80,8 @@ export const header_with_url = [
     header: "Raw Material Inventory",
     // url: "https://fabricpro.netlify.app",
      // url:"https://fabricprov1.netlify.app/",
-     url:"https://fabricprov3.netlify.app",
+     // url:"https://fabricprov3.netlify.app",
+     url:"https://fabricprov4.netlify.app",
     icon: <FaBoxes />,
   },
    {
